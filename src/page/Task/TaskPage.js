3e40547b9deba4dc7ec0aed92af627ab7769046f
lam00 <lam009@qq.com>
2018-06-29 20:10:08 +0800

    添加了ListView的九宫格效果

--- src/page/Task/TaskPage.js
+++ src/page/Task/TaskPage.js
@@ -3,10 +3,12 @@ import {
     View,
     Text,
     Image,
-    StyleSheet
+    StyleSheet,
+    ScrollView
 } from 'react-native';
 import SwiperComponent from "../../common/SwiperComponent";
 import ListViewComponent from "../../common/listViewComponent";
+import ListViewComponent2 from "../../common/listViewComponent2";
 
 export default class TaskPage extends Component {
     static navigationOptions = {
@@ -25,16 +27,23 @@ export default class TaskPage extends Component {
 
     render() {
         return (
-            <View>
-                <SwiperComponent/>
-                <Text style={styles.textStyle}>基础任务</Text>
-                <ListViewComponent/>
-            </View>
-
+            <ScrollView style={styles.container}>
+                <View>
+                    <SwiperComponent/>
+                    <Text style={styles.textStyle}>基础任务</Text>
+                    <ListViewComponent/>
+                    <Text style={styles.textStyle}>独家任务</Text>
+                    <ListViewComponent2/>
+                </View>
+            </ScrollView>
         );
     }
 }
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F8F8F8'
+    },
     textStyle: {
         fontSize: 15,
         color: '#000',
